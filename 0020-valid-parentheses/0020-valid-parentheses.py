d6fa0a2a5fb33fa40247e2027a092dd3ddcdd189
Zack Zhao <111853191+ZhixiaZhao@users.noreply.github.com>
2023-04-24 20:33:59 +1000

    Time: 34 ms (49.04%), Space: 13.9 MB (56.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,12 +5,13 @@ class Solution:
             if i in "({[":
                 brackets_check.append(i)
             else:
+                 # list is empty but has closing bracket
                 if len(brackets_check) == 0 or \
                 (i == ')' and brackets_check[-1] != '(') or \
                 (i == ']' and brackets_check[-1] != '[') or \
                 (i == '}' and brackets_check[-1] != '{'):
                     return False
                 brackets_check.pop()
-        return not brackets_check
+        return not brackets_check # loop all strings and matches
     
     
