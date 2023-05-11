03ec19e4f1f985f7cc7990111d84f2ec1b37fef3
Zack Zhao <111853191+ZhixiaZhao@users.noreply.github.com>
2023-05-11 23:03:55 +1000

    Time: 2223 ms (32.46%), Space: 20.5 MB (12.23%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -16,9 +16,9 @@ class Solution:
                     result.append([nums[i], nums[left], nums[right]])
                     left += 1
                     right -= 1
-                    while left < right and nums[left] == nums[left - 1]:
+                    while left < right and nums[left] == nums[left - 1]: # watch out left - 1
                         left += 1
-                    while left < right and nums[right] == nums[right + 1]:
+                    while left < right and nums[right] == nums[right + 1]: # watch out right + 1
                         right -= 1
                 
                 elif nums[i] + nums[left] + nums[right] > 0:
