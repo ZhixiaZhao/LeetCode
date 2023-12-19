df3353699f658b57e1907a45de6725cf23f6e8b9
Zack Zhao <111853191+ZhixiaZhao@users.noreply.github.com>
2023-12-19 16:00:08 +1000

    Time: 50 ms (28.63%), Space: 16.7 MB (40.32%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -4,6 +4,16 @@ class Solution:
         length = len(citations)
         h = []
         for i, num in enumerate(citations):
+            # h = max(h, min(num, length - i))
             if num >= length - i:
                 return length - i
         return 0
+'''
+There are three conditions:
+1). If the array contains only one element and it is greater than zero, the h-index is 1.
+2). If the smallest value in the sorted array is greater than or equal to the length of the array, the h-index is equal to the length of array.
+3). 
+
+
+ Otherwise, the array is traversed, and for each index i, the value at that index is compared to i+1. If the value is less than i+1, i is returned as the h-index. If none of the conditions are met, the h-index is 0.
+'''
