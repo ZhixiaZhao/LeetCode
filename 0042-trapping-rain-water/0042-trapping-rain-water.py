e1450fa24dddfcd50a71a9aeaa2861aff6da03f2
Zack Zhao <111853191+ZhixiaZhao@users.noreply.github.com>
2024-01-17 17:58:19 +1000

    Time: 93 ms (96.14%), Space: 19.4 MB (19.97%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,5 +1,38 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        
+        # Two pointers
+        left = 0
+        right = len(height) - 1
+        
+        maxLeft = height[left]
+        maxRight = height[right]
+        result = 0
+        
+        while left < right:
+            if maxLeft < maxRight:
+                left += 1
+                maxLeft = max(maxLeft, height[left])
+                result += (maxLeft - height[left])
+            else:
+                right -= 1
+                maxRight = max(maxRight, height[right])
+                result += (maxRight - height[right])
+        return result
+        
+        
+        
+        
+        
+        '''
+        # Two traverses
+        # original                 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1
+        # max left                 0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3
+        # max right                3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 1, 1
+        # min(max left, max right) 0, 0, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1
+        
         maxLeftHeight = []
         maxRightHeight = []
         minLeftRight = []
@@ -24,4 +57,6 @@ class Solution:
             if h > 0:
                 result += h
         
-        return result    
+        return result
+        '''
+        
