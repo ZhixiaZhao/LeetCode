0f57137585d2fcd12a693a2c47e4282ef79fef53
Zack Zhao <111853191+ZhixiaZhao@users.noreply.github.com>
2024-01-18 10:32:41 +0800

    Time: 58 ms (30.45%), Space: 17.3 MB (23.87%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,12 +1,24 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        intToRoman = {1: "I", 4: "IV", 5: "V", 10: "x", 50: "L", 100: "C", 500: "D", 1000: "M"}
+        int_map = {1: "I", 4: "IV", 
+                      5: "V", 9: "IX", 
+                      10: "X", 40: "XL", 
+                      50: "L", 90: "XC", 
+                      100: "C", 400: "CD", 
+                      500: "D", 900: "CM", 
+                      1000: "M"}
         result = ""
-    
-        thounds = num // 1000 # 千分位
-        hundreds = (num % 1000) // 100 # 百分位
-        tens = (num % 100) // 10 # 十分位
-        ones = num % 10 # 个位
+        
+        for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            while n <= num:
+                result += int_map[n]
+                num -= n
+        return result
+        '''
+        thounds = num // 1000 
+        hundreds = (num % 1000) // 100 
+        tens = (num % 100) // 10 
+        ones = num % 10 
     
         if thounds:
             result += thounds * "M"
@@ -49,4 +61,4 @@ class Solution:
         
         
         return result
-        
+        '''
