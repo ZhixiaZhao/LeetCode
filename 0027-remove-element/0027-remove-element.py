215ddd78c66ac9e38650002fd29c121c81ed65cb
Zack Zhao <111853191+ZhixiaZhao@users.noreply.github.com>
2024-03-12 11:25:18 +1000

    Time: 31 ms (91.11%), Space: 16.5 MB (56.58%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,10 +1,22 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        '''
         k = 0
         for i in range(len(nums)):
             if nums[i] != val:
                 nums[k] = nums[i]
                 k += 1
         return k
+        '''
         
+        # Two Pointers
+        fast = 0 # find the element in the new arreay
+        slow = 0 # update the position in the new array
+        
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[slow] = nums[i]
+                slow += 1
+            fast += 1
+        return slow
         
