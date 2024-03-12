7239c78ffdb8f009b51c8f552bb1b4dbeed9ad07
Zack Zhao <111853191+ZhixiaZhao@users.noreply.github.com>
2024-03-12 10:48:34 +1000

    Time: 185 ms (88.34%), Space: 18 MB (85.98%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -11,19 +11,36 @@ class Solution:
         # Binary search 
         
         # [left, right]
-        left = 0
-        right = len(nums) - 1
+#         left = 0
+#         right = len(nums) - 1
         
-        while left <= right:
+#         while left <= right:
+#             middle = (left + right) // 2
+            
+#             if nums[middle] > target:
+#                 right = middle - 1
+#             elif nums[middle] < target:
+#                 left = middle + 1
+#             else:
+#                 return middle
+#         return -1
+    
+        # [left, right)
+        left = 0
+        right = len(nums)
+        
+        while left < right:
             middle = (left + right) // 2
             
             if nums[middle] > target:
-                right = middle - 1
+                right = middle
             elif nums[middle] < target:
                 left = middle + 1
             else:
                 return middle
         return -1
+        
+        
             
         
         
