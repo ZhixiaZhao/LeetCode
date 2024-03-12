2fb5c34543c258de62e19a4f6bd7c32edeb78f2c
Zack Zhao <111853191+ZhixiaZhao@users.noreply.github.com>
2024-03-12 10:45:55 +1000

    Time: 189 ms (73.20%), Space: 18 MB (85.98%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,7 +1,29 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        '''
         if target not in nums:
             return -1
         else:
             return nums.index(target)
+        
+        '''
+        
+        # Binary search 
+        
+        # [left, right]
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            middle = (left + right) // 2
+            
+            if nums[middle] > target:
+                right = middle - 1
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                return middle
+        return -1
+            
+        
         
