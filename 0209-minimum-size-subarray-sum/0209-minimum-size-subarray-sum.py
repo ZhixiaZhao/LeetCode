2e6ec820604573259d1cbc85f7d8b9377dddac5e
Zack Zhao <111853191+ZhixiaZhao@users.noreply.github.com>
2024-03-12 12:17:50 +1000

    Time: 206 ms (22.47%), Space: 30.4 MB (43.87%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        '''
         if sum(nums) < target:
             return 0
         else:
@@ -18,4 +19,28 @@ class Solution:
         # if the target is less than target, move right pointer.
         # if the target is still greater than target, move left pointer to find the minimum
         # size.
+        '''
+        
+        # slide window, like two pointers
+        if sum(nums) < target:
+            return 0
+        else:
+            length = len(nums) # the total length of nums
+            right = 0 # right index of the slide window
+            left = 0 # left index of the slide window
+            total = 0 # sum of elements in slide window
+            min_len = len(nums) # minimum length of slide window
+            
+            while right < length:
+                total += nums[right]
+                
+                while total >= target:
+                    min_len = min(min_len, right - left + 1)
+                    total -= nums[left]
+                    left += 1
+                
+                right += 1
+                
+            return min_len
+            
         
