ec4e641a05be5f220b19364aacd09996cc4b0c93
Zack Zhao <111853191+ZhixiaZhao@users.noreply.github.com>
2024-03-14 12:16:06 +1000

    Time: 49 ms (55.92%), Space: 19.5 MB (59.51%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -10,19 +10,34 @@ class Solution:
         # 2). val is at the head position
         # 3). current node is not null and next node is not null
         
-        if not head :
-            return head
-        while head.val == val:
-            if head.next:
-                head = head.next
-            else:
-                return 
-        cur = head
+        # # not use dummy head
+        # if not head :
+        #     return head
+        # while head.val == val:
+        #     if head.next:
+        #         head = head.next
+        #     else:
+        #         return 
+        # cur = head
+        # while cur.next:
+        #     if cur.next.val == val:
+        #         cur.next = cur.next.next
+        #     else:
+        #         cur = cur.next
+        # return head
+    
+        # use dummy head
+        dummyHead = ListNode(0)
+        dummyHead.next = head
+        
+        cur = dummyHead # not dummyHead.next, because we must know the previous Next 
+                        # if we  want to delete current node
         while cur.next:
             if cur.next.val == val:
                 cur.next = cur.next.next
             else:
                 cur = cur.next
-        return head
+        return dummyHead.next
+    
         
         
